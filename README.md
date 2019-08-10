0b69cb198cceb149c5384af12e928d269963dd47
keita miyano <keita.miyano@estie.co.jp>
2019-08-10 17:32:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# connect_mysql
+
+mysqlにpythonで繋ぐためのmodule
+
+# install
+
+```shell
+pip install -U connect_mysql
+```
+
+# usage
+
+```python
+from connect_mysql import Connector
+connector = Connector(host, db_name, user_name, password)
+print(connecor.tablelist) # テーブルリストの取得
+df = connector.get_query("SELECT * FROM sample_table;")
+```
---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="connect_mysql",
+    version="0.0.1",
+    python_requires=">=3.5",
+    packages=find_packages(),
+    install_requires=_requires_from_file('requirements.txt'),
+    url="https://github.com/mynkit/connect_mysql",
+)
